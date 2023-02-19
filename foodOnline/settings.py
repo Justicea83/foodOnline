38090bb89eb0245797c806fba7a7b42a6a1f1f50
Justice Arthur <justicea83@gmail.com>
2023-02-19 23:06:37 +0000

    add email config

--- foodOnline/settings.py
+++ foodOnline/settings.py
@@ -135,7 +135,12 @@ MEDIA_URL = "/media/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# Email Config
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
